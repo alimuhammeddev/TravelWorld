d650aff8437056f95eb653f9e78e20179282ed53
alimuhammed_Dev <muhammedaliali2020@gmail.com>
2025-08-14 18:48:41 +0100

    finished working on testimonial

--- src/Components/Testimonial.jsx
+++ src/Components/Testimonial.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { customer1 } from '../assets'
+import { customer1, customer2, customer3 } from '../assets'
 
 const Testimonial = () => {
     return (
@@ -9,13 +9,33 @@ const Testimonial = () => {
                 <h1 className='text-[#0B2727] font-medium lg:text-3xl text-xl lg:mt-3 mt-2'>What our customers say about us</h1>
             </div>
 
-            <div>
-                <div>
+            <div className='grid lg:grid-cols-3 grid-cols-1 gap-5 lg:mt-10 mt-5'>
+                <div className='border-2 lg:border-none lg:p-0 border-[#FAA935] p-5 rounded-xl'>
                     <h1 className='text-gray-400 lg:text-lg'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus sit, explicabo provident hic distinctio molestias voluptates nobis alias placeat suscipt earum debitits recusandae voluptate illum expedita corrupti aliquid doloribus delectus?</h1>
-                    <div className='flex items-center gap-5'>
+                    <div className='flex items-center gap-5 mt-5'>
                         <img src={customer1} alt="" />
                         <div>
                             <h1 className='text-[#0B2727] font-medium'>John Deep</h1>
+                            <p className='text-gray-400'>Customer</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='border-2 lg:border-none lg:p-0 border-[#FAA935] p-5 rounded-xl'>
+                    <h1 className='text-gray-400 lg:text-lg'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus sit, explicabo provident hic distinctio molestias voluptates nobis alias placeat suscipt earum debitits recusandae voluptate illum expedita corrupti aliquid doloribus delectus?</h1>
+                    <div className='flex items-center gap-5 mt-5'>
+                        <img src={customer2} alt="" />
+                        <div>
+                            <h1 className='text-[#0B2727] font-medium'>Ally Gomez</h1>
+                            <p className='text-gray-400'>Customer</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='border-2 lg:border-none lg:p-0 border-[#FAA935] p-5 rounded-xl'>
+                    <h1 className='text-gray-400 lg:text-lg'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus sit, explicabo provident hic distinctio molestias voluptates nobis alias placeat suscipt earum debitits recusandae voluptate illum expedita corrupti aliquid doloribus delectus?</h1>
+                    <div className='flex items-center gap-5 mt-5'>
+                        <img src={customer3} alt="" />
+                        <div>
+                            <h1 className='text-[#0B2727] font-medium'>Alex Bugg</h1>
                             <p className='text-gray-400'>Customer</p>
                         </div>
                     </div>
