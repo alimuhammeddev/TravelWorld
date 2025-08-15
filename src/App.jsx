0624ce84da068d47857e3cfda42553da238327ac
alimuhammed_Dev <muhammedaliali2020@gmail.com>
2025-08-15 11:50:46 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Tours from "./Pages/Tours";
+import ScrollToTop from "./ScrollToTop";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/tours" element={<Tours />} />
-    </Routes>
+    <div>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tours" element={<Tours />} />
+      </Routes>
+    </div>
   );
 }
 
