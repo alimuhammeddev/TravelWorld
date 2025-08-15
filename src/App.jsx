624e657522438be246b28c45d871e7c7125c3bda
alimuhammed_Dev <muhammedaliali2020@gmail.com>
2025-08-15 11:08:42 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import { Route, Routes } from "react-router-dom"
-import Home from './Pages/Home'
-import Tours from './Pages/Tours'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Tours from "./Pages/Tours";
 
-
-const App = () => {
+function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/tours" element={<Tours />} />
       </Routes>
-    </div>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
